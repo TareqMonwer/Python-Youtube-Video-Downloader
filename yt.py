0dd51c9a6e9514bd942c47e052c1758efa5504e9
Tareq Monwer <tareqmonwer137@gmail.com>
2019-02-26 03:32:47 +0600

    New features added
    Some bugs are removed and messages aren't in console anymore.
    Now you can choose you destination folder from GUI.
    There is no need to edit the code, Just run and get your file downloaded!

--- yt.py
+++ yt.py
@@ -1,47 +1,57 @@
-'''
-import pytube
-print("Enter the video link")
-link = input()
-
-yt = pytube.YouTube(link)
-
-stream = yt.streams.first()
-
-
-
-print("You file will be saved in Downloads")
-destination = '/home/tareq/Downloads'
-stream.download(destination)
-
-print("\nVideo has been downloaded")
-'''
-
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 import pytube
 
+"""
+This GUI tool will download youtube
+videos from given link. Feel free to
+customize it if you're a pythonist
+"""
 
-# Functions
+# Functions section
+class File:
+    ''' This function will set download path '''
+    def des(self):
+        fp = filedialog.askdirectory()
+        return fp
 
 def dload():
+    '''
+    This callback function will run when
+    download button is clicked
+    '''
+    
     try:
+        lb1.insert(END, 'Download started')
+        dest = File()
         link = link_txt.get()
         yt = pytube.YouTube(link)
         stream = yt.streams.first()
-        destination = '/home/tareq/Downloads'
+        destination = dest.des()
+        lb2.insert(END, destination)
         stream.download(destination)
-        print("Downloading")
+        lb1.insert(END, 'Download Completed')
+        return True
     except:
-        print("something is wrong")
-    
-    
+        lb1.insert(END, 'Download Interrupted')
+
+def close():
+    ''' This function will close the application '''
+    window.destroy()
+
 
 
 window = Tk()
+window.title("YouTube Video Downloader")
+
 
 # Labels
 l1 = Label(window, text="Video Link")
 l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Destination Folder")
+l2.grid(row=1, column=0)
 
 
 # Entry
@@ -49,32 +59,33 @@ link_txt = StringVar()
 e1 = Entry(window, textvariable=link_txt)
 e1.grid(row=0, column=1)
 
+
 # Button
 b1 = Button(window, text="Download", command=dload)
 b1.grid(row=0, column=2)
 
+closebtn = Button(window, text="Close", fg='red', command=close)
+closebtn.grid(row=6, column=2)
+
+f = File()
+des = Button(text="Choose", command=f.des)
+des.grid(row=1, column= 2)
+
+
 # Seperator
 s1 = ttk.Separator(window, orient=HORIZONTAL,).grid(row=2, columnspan=5, sticky="ew")
 
-# Text
-txt1 = Text(window, height=2, width=40, fg='red')
-txt1.grid(row=3, columnspan=5)
-txt1.insert(END, "Your file will be saved in Downloads\nThanks for using my tool\n")
-txt1.configure(state="disabled")
 
-stat = StringVar()
-txt2 = Text(window, height=2, width=40, fg='green')
-txt2.grid(row=4, columnspan=5)
-txt2.insert(END, stat)
+# Listbox
+lb1 = Listbox(window, height=3, width=30)
+lb1.grid(row=5, column=1)
 
 
-
-
-
+lb2 = Listbox(window, height=1)
+lb2.grid(row=1, column= 1)
 
 
 window.mainloop()
-
 
 
 
